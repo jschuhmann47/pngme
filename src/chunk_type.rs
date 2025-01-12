330e64cf8845325d6c043577b34dae3657b7ca02
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2025-01-12 18:56:37 -0300

    impl from str

--- src/chunk_type.rs
+++ src/chunk_type.rs
@@ -1,31 +1,44 @@
+use std::str::FromStr;
 
-
+#[derive(PartialEq, Eq, Debug)]
 struct ChunkType {
-    bytes: [i32; 4]
+    bytes: [u8; 4],
 }
 
 impl ChunkType {
-    fn bytes(&self) -> [i32; 4] {
+    fn bytes(&self) -> [u8; 4] {
         self.bytes
     }
-
-    fn try_from(bytes: [i32; 4]) -> Result<ChunkType, &'static str> {
-        if bytes.iter().any(|b| !is_character(*b)) {
-            return Err("invalid chunk type");
-        }
-        Ok(ChunkType { bytes: bytes })
-
-    }
-
-    
 }
 
-fn is_character(byte: i32) -> bool {
+impl TryFrom<[u8; 4]> for ChunkType {
+    type Error = String;
+
+    fn try_from(value: [u8; 4]) -> Result<Self, Self::Error> {
+        if value.iter().any(|b| !is_character(*b)) {
+            return Err(String::from("invalid chunk type"));
+        }
+        Ok(ChunkType { bytes: value })
+    }
+}
+fn is_character(byte: u8) -> bool {
     (byte >= 97 && byte <= 122) || (byte >= 65 && byte <= 90)
 }
 
+impl FromStr for ChunkType {
+    type Err = String;
 
-
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.len() != 4 {
+            return Err(String::from("invalid str length"));
+        }
+        let mut bytes: [u8; 4] = [0; 4];
+        for (i, c) in s.bytes().enumerate() {
+            bytes[i] = c
+        }
+        Ok(ChunkType { bytes })
+    }
+}
 
 #[allow(unused_variables)]
 #[cfg(test)]
@@ -42,4 +55,10 @@ mod tests {
         assert_eq!(expected, actual.bytes());
     }
 
+    #[test]
+    pub fn test_chunk_type_from_str() {
+        let expected = ChunkType::try_from([82, 117, 83, 116]).unwrap();
+        let actual = ChunkType::from_str("RuSt").unwrap();
+        assert_eq!(expected, actual);
+    }
 }
